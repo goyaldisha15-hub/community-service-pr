6309486e295b2eff23fbcd380c0a90474444c0fb
Adarsh78273 <adarsh103107sharma@gmail.com>
2026-09-21 12:13:42 +0530

    hello

--- src/pages/volunteer/VolunteerDashboardPage.jsx
+++ src/pages/volunteer/VolunteerDashboardPage.jsx
@@ -2,3 +2,5 @@ import { Link } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
 import { useEvents } from '../../context/EventContext'
 export default function VolunteerDashboardPage() { const { user } = useAuth(); const { registrations } = useEvents(); const mine = registrations.filter((item) => item.volunteerId === user.id); return <section><h1>Welcome, {user.name}</h1><div className="stats"><article><b>{mine.length}</b><span>Registered events</span></article><article><b>{mine.filter((item) => item.attended).length}</b><span>Events attended</span></article></div><Link className="primary-link" to="/volunteer/events">Browse events</Link></section> }
+
+// hello
